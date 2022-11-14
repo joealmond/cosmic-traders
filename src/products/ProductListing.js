e1b5c5d1d803336d3a18c25d2c2b3bd9f3ffcd7f
József Mandula <jozsef.mandula@gmail.com>
2022-11-14 19:15:25 +0100

    21 Accessing API Data with useEffect - done last

--- src/products/ProductListing.js
+++ src/products/ProductListing.js
@@ -1,36 +1,55 @@
-import { useSearchParams } from 'react-router-dom'
+import { useSearchParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-import products from '../products/products.json'
-import styles from './ProductListing.module.scss'
-import Product from './Product'
+// import products from '../products/products.json'
+import styles from "./ProductListing.module.scss";
+import Product from "./Product";
 
 function ProductListing(props) {
-  const [searchParams, setSearchParams] = useSearchParams()
-  const filter = searchParams.get('filter')
+  const [searchParams, setSearchParams] = useSearchParams();
+  const filter = searchParams.get("filter");
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/products") // find proxy in package.json
+      .then((res) => res.json())
+      .then((productsArray) => setProducts(productsArray));
+  }, []);
 
   function setFilter(filter) {
-    setSearchParams({ filter })
+    setSearchParams({ filter });
   }
 
   return (
     <>
       <div className={styles.filters}>
-        <button className="button" onClick={() => setFilter('transport')}>Transport</button>
-        <button className="button" onClick={() => setFilter('weapons')}>Weapons</button>
-        <button className="button" onClick={() => setFilter('defense')}>Defense</button>
-        <button className="button" onClick={() => setFilter('equipment')}>Equipment</button>
-        <button className="button" onClick={() => setFilter('')}>All</button>
+        <button className="button" onClick={() => setFilter("transport")}>
+          Transport
+        </button>
+        <button className="button" onClick={() => setFilter("weapons")}>
+          Weapons
+        </button>
+        <button className="button" onClick={() => setFilter("defense")}>
+          Defense
+        </button>
+        <button className="button" onClick={() => setFilter("equipment")}>
+          Equipment
+        </button>
+        <button className="button" onClick={() => setFilter("")}>
+          All
+        </button>
       </div>
       <ul className={styles.products}>
         {products
-          .filter(p => !filter || p.category === filter)
+          .filter((p) => !filter || p.category === filter)
           .map((product, idx) => (
-            <li key={idx} >
+            <li key={idx}>
               <Product productData={product} addToCart={props.addToCart} />
-            </li>)
-          )}
+            </li>
+          ))}
       </ul>
     </>
-  )
+  );
 }
-export default ProductListing
+export default ProductListing;
