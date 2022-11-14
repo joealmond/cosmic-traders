58ef6770d7c97f170039993639c562283f6fddd7
József Mandula <jozsef.mandula@gmail.com>
2022-11-14 12:26:23 +0100

    17 Linking to routes - done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import Outpost from "./outpost/Outpost";
+import Checkout from "./checkout/Checkout";
+import Header from "./header/Header";
 import styles from "./App.module.scss";
 
 function App() {
   return (
-    <div className={styles.content}>
-      <Outpost />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className={styles.content}>
+        <Routes>
+          <Route path="/outpost" element={<Outpost />}></Route>
+          <Route path="/checkout" element={<Checkout />}></Route>
+          <Route path="/" element={<Navigate to="/outpost" />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
